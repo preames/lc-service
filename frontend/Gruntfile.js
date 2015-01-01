6a8eab68910af32a4da565930d555aebfb0b4717
Richard <legalize@xmission.com>
2015-01-01 16:54:27 -0700

    Add proxy server configuration

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -40,9 +40,40 @@ module.exports = function (grunt) {
     connect: {
       main: {
         options: {
-          port: 9001
+          port: 9001,
+          middleware: function (connect, options) {
+            if (!Array.isArray(options.base)) {
+                options.base = [options.base];
+            }
+
+            // Setup the proxy
+            var middlewares = [require('grunt-connect-proxy/lib/utils').proxyRequest];
+
+            // Serve static files.
+            options.base.forEach(function(base) {
+              middlewares.push(connect.static(base));
+            });
+
+            // Make directory browse-able.
+            var directory = options.directory || options.base[options.base.length - 1];
+            middlewares.push(connect.directory(directory));
+
+            return middlewares;
+          }
         }
-      }
+      },
+      proxies: [
+        {
+          context: '/api',
+          host: 'localhost',
+          port: 8000,
+          https: false,
+          changeOrigin: false,
+          xforward: false,
+          headers: {
+          }
+        }
+      ]
     },
     watch: {
       main: {
@@ -195,7 +226,7 @@ module.exports = function (grunt) {
   });
 
   grunt.registerTask('build',['jshint','clean:before','less','dom_munger','ngtemplates','cssmin','concat','ngmin','uglify','copy','htmlmin','imagemin','clean:after']);
-  grunt.registerTask('serve', ['dom_munger:read','jshint','connect', 'watch']);
+  grunt.registerTask('serve', ['dom_munger:read', 'jshint', 'configureProxies:server', 'connect:main', 'watch']);
   grunt.registerTask('test',['dom_munger:read','karma:all_tests']);
 
   grunt.event.on('watch', function(action, filepath) {
