c510b7c55029e02f866bca5ade8e0c76a6557166
Philip Reames <reames@eecs.berkeley.edu>
2015-01-01 12:34:56 -0800

    Introduce the most basic possible models.  One table to hold information about the requests themselves, and then another one to fake a message queue.  This later is horrible, but helps get something working quicker.

--- service/api/models.py
+++ service/api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Request(models.Model):
+    # implicit 'id' primary key field
+    datetime = models.DateTimeField('date requested')
+    repo = models.URLField(max_length=512)
+
+    def __str__(self):
+        return ", ".join([str(self.id), 
+                          str(self.datetime), 
+                          str(self.repo)])
+
+class LogMessage(models.Model):
+    # implicit 'id' primary key field
+    request = models.ForeignKey(Request)
+    datetime = models.DateTimeField('date published')
+    payload = models.TextField('json payload')
+
+    def __str__(self):
+        return ", ".join([str(self.id), 
+                          str(self.request), 
+                          str(self.datetime), 
+                          str(self.payload)])
+
--- service/api/views.py
+++ service/api/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-# Create your views here.
+from models import *
+import json
+import datetime
 
 # By default, any unmapped URL hits this and gets a page not found
 def index(request):
@@ -15,7 +17,18 @@ def start(request):
     tainted_repo = request.REQUEST["repository"]
     # TODO: validate the repository is actual a github url
     # TODO: validate it's in our whitelist (for now)
-    return HttpResponse("START " + tainted_repo)
+    # First, create a record in the request table for this job, then
+    # actually send a message to the job server to request it be run
+    request = Request.objects.create(datetime=datetime.datetime.now(), 
+                                     repo=tainted_repo)
+
+    message_dict = {"action": "job_start"}
+    message_json = json.dumps(message_dict)
+    message = LogMessage.objects.create(request=request, 
+                                        datetime=datetime.datetime.now(),
+                                        payload=message_json)
+
+    return HttpResponse("START " + tainted_repo + " --> " + str(request.id))
 
 # Poll for the current status of an existing request
 def status(request):
@@ -25,7 +38,17 @@ def status(request):
         raise Http404;
     tainted_id = request.REQUEST["id"]
     # TODO: validate user permissions to this id 
-    return HttpResponse("POLL " + tainted_id)
+    request = get_object_or_404(Request, id=tainted_id)
+
+    # Find the last message related to this request.  We're taking advantage
+    # of knowing that this table is a write only log and thus primary key
+    # implies time ordering
+    messages = LogMessage.objects.all().filter(request=request)
+    last = messages.order_by('pk').reverse()[0:1]
+    if not last:
+        return HttpResponse("POLL " + tainted_id + "No Messages")
+    payload = last.payload
+    return HttpResponse("POLL " + tainted_id + str(payload))
 
 # Stop an existing request
 def stop(request):
@@ -35,4 +58,15 @@ def stop(request):
         raise Http404;
     tainted_id = request.REQUEST["id"]
     # TODO: validate user permissions to this id 
-    return HttpResponse("STOP " + tainted_id)
+    request = get_object_or_404(Request, id=tainted_id)
+
+    # TODO: Check to see if a stop request already sent, report an error
+
+    message_dict = {"action": "job_stop"}
+    message_json = json.dumps(message_dict)
+    message = LogMessage.objects.create(request=request, 
+                                        datetime=datetime.datetime.now(),
+                                        payload=message_json)
+
+
+    return HttpResponse("STOP " + tainted_id + ", " + str(request.id))
